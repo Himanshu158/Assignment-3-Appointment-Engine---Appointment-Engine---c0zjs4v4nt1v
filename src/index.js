1772ff4f37d683754eec33add9463ffa7224bce3
Himanshu <mailboxofhimanshu1999@gmail.com>
2022-02-10 20:29:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 8080
 
-// Parse JSON bodies (as sent by API clients)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-const  con  = require('./connector')
+const con = require("./connector");
+const { slot_data,data } = require('./data')
 
+const refreshAll = async () => {
 
+    await con.query("DROP TABLE IF EXISTS events, user;",
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    await con.query("CREATE TABLE events (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL,slot1 INT, slot2 INT, slot3 INT, slot4 INT, slot5 INT, slot6 INT, slot7 INT, slot8 INT, slot9 INT, slot10 INT,slot11 INT, slot12 INT, slot13 INT, slot14 INT, primary key (id));",
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        });
 
-module.exports = app;
+    await con.query("CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name TEXT(65535), primary key (id));",
+    (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+    });
+    var i = 1;
+    await slot_data.forEach(element => {
+        i++;
+        con.query("INSERT INTO events VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", [i].concat(Object.values(element)), (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+    i = 1;
+    await data.forEach(element => {
+        i++;
+        con.query("INSERT INTO user  VALUES (?,?);",[i].concat(Object.values(element)), (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+    console.log("data refreshed");
+}
+refreshAll();
